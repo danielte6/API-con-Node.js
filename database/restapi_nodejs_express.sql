f927e8271893a89f6b766bd3e9ad6b69e5eb1420
richard.valdez <152217705+danielte6@users.noreply.github.com>
2023-11-28 06:47:23 -0500

    Update restapi_nodejs_express.sql

--- database/restapi_nodejs_express.sql
+++ database/restapi_nodejs_express.sql
@@ -1,69 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-03-2022 a las 03:22:10
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+CREATE TABLE `alimento` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombredelproducto` VARCHAR(30) COLLATE utf8_general_ci NOT NULL,
+  `preciodelproducto` INT(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Volcar datos para la tabla `alimento`
+INSERT INTO `alimento` (`id`, `nombredelproducto`, `preciodelproducto`) VALUES
+(1, 'arroz', 2);
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `restapi_nodejs_express`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `language`
---
-
-CREATE TABLE `language` (
-  `id` tinyint(2) UNSIGNED NOT NULL,
-  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `programmers` tinyint(2) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Store the programming languages.';
-
---
--- Volcado de datos para la tabla `language`
---
-
-INSERT INTO `language` (`id`, `name`, `programmers`) VALUES
-(1, 'JavaScript', 12),
-(2, 'Python', 21),
-(4, 'PHP', 50),
-(6, 'C# .NET', 45);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `language`
---
-ALTER TABLE `language`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `language`
---
-ALTER TABLE `language`
-  MODIFY `id` tinyint(2) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- AUTO_INCREMENT para la tabla `alimento`
+ALTER TABLE `alimento` MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
